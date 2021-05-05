1233ad0b73d61ba812b1712f363c41d2d7a65655
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-05-05 12:41:40 +0200

    modal-added

---
+++ assets/js/modal.js
@@ -0,0 +1,10 @@
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+$('#myModalChair').on('shown.bs.modal', function () {
+  $('#myInputchair').trigger('focus')
+})
+$('#myModalCouch').on('shown.bs.modal', function () {
+  $('#myInputcouch').trigger('focus')
+})
